0082a79e02a59ba9846e4eb57e520a7eaef3facb
Thiago Spart <user000.000resu@gmail.com>
2020-10-30 14:54:41 -0300

    Meus programas em JAVA
    Uppando projeto do eclipse

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package fundamentos;
+/**
+ * 
+ * @author Thiago
+ *
+ */
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		//SYSO
+		System.out.println("First Program!!!");
+	}
+}
+//This is a one line comment in java
+/*
+	and this is for two or more lines 
+	if you don't have time to input commits maybe you should use the Ctrl + / this will commits all of the selected lines
+*/
